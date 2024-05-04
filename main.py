ee4ce9df125aa11eb85003fa76988a2d045d709f
JuarezWeitzel <juarez.zbw@gmail.com>
2024-05-04 16:37:21 -0300

    feat: troca thema, configura opacidade, configura mana_potion, configura botao limpar, configura botao save e load.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from tkinter.ttk import Label, Button, Combobox, Style
+from tkinter import messagebox
 from ttkthemes import ThemedTk
 from size_window import center_window
 from PIL import Image, ImageTk
+import pyautogui
+import keyboard
+import json
 
-root = ThemedTk(theme='arc', themebg=True)
+from window import hidden_client
+
+root = ThemedTk(theme='black', themebg=True)
 root.title("MyInterface")
 root.resizable(False, False)
 style = Style()
 style.configure('TButton', font=("Roboto", 12))
+style.configure('Ativado.TButton', foreground="green")
+style.configure('Desativado.TButton', foreground="red")
 
 # Definir as dimensões desejadas da janela
 width = 290
@@ -34,19 +42,66 @@ lbl_cast = generete_widget(Label, row=1, column=0, text="HotKey Cast", sticky="W
 cbx_cast = generete_widget(Combobox, row=1, column=1, padx=30, pady=10, values=HOTKEYS, state="readonly", font=("Roboto", 12), width=8)
 cbx_cast.current(0)
 
-btn_mana_position = generete_widget(Button, row=2, column=0, sticky="NSEW", text="Mana Potion")
+rgb = ""
+mana_position = ""
+
+def get_mana_position():
+    global rgb
+    global mana_position
+    messagebox.showinfo(title="Mana Position", message="Posicione o mouse em cima da barra de mana e pressione a tecla insert.")
+    keyboard.wait('insert')
+    x, y = pyautogui.position()
+    rgb = pyautogui.screenshot().getpixel((x, y))
+    messagebox.showinfo(title='Mana Result', message=f"X: {x} Y: {y} - RGB: {rgb}")
+    lbl_mana_position.configure(text=f"({x}, {y})")
+    mana_position = [x, y]
+     
+btn_mana_position = generete_widget(Button, row=2, column=0, sticky="NSEW", text="Mana Potion", command=get_mana_position)
 lbl_mana_position = generete_widget(Label, row=2, column=1, text="Empty", font=("Roboto", 12), sticky="W")
 
 trash = load_trash()
-btn_mana_position_trash = generete_widget(Button, row=2, column=1, image=(trash), sticky="E")
+
+def clear():
+    lbl_mana_position.configure(text="Empty")
+
+btn_mana_position_trash = generete_widget(Button, row=2, column=1, image=(trash), sticky="E", command=clear)
 
 cbx_food = generete_widget(Combobox, row=0, column=1, padx=30, pady=10, values=HOTKEYS, state="readonly", font=("Roboto", 12), width=8)
 cbx_food.current(0)
 
-btn_opacity = generete_widget(Button, row=3, column=0, text="Apply Opacity", columnspan=2)
+def opacity():
+    result = hidden_client()
+    if result == 1:
+        btn_opacity.configure(style="Ativado.TButton")
+        return
+    btn_opacity.configure(style="Desativado.TButton")
 
-btn_start = generete_widget(Button, row=4, column=0, text="START")
+btn_opacity = generete_widget(Button, row=3, column=0, text="Apply Opacity", columnspan=2, command=opacity)
 
-btn_load = generete_widget(Button, row=4, column=1, text="LOAD")
+def save():
+    my_data = {
+        "food": {
+           "value": cbx_food.get(),
+           "position": cbx_food.current()
+        },
+        "spell": {
+           "value": cbx_cast.get(),
+           "position": cbx_cast.current()
+        },
+        "mana_pos": {
+            "position": mana_position,
+            "rgb": rgb
+        }
+    }
+    with open("infos.json", "w") as file:
+        file.write(json.dumps(my_data))
+
+btn_start = generete_widget(Button, row=4, column=0, text="START", command=save)
+
+def load():
+    with open("infos.json", "r") as file:
+        data = json.loads(file.read())
+
+btn_load = generete_widget(Button, row=4, column=1, text="LOAD", command=load)
 
 root.mainloop()
